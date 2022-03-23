7ecc42b3005efb81b91198d2e6e91e1e04089651
abiari <abiari@student.1337.ma>
2022-03-23 15:18:42 +0100

    mini search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>{{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, ref, watch } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const search = ref('')
+    const names = ref(['aabounak','abiari','mashad','mgrissen','oel-ouar','oel-yous', 'ztaouil'])
+
+    watch(search, () => {
+      
+    })
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.startsWith(search.value))
+    })
+    return { names, search, matchingNames }
   }
 }
 </script>
